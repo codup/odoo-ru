a1397577c8945eb03636150db5085436dee00199
wowas18 <wowas18@nm.ru>
2015-04-16 08:37:54 +0400

    email TORG-12 instead invoice

--- l10n_ru_doc/account_invoice.py
+++ l10n_ru_doc/account_invoice.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2014 CodUP (<http://codup.com>).
+#    Copyright (C) 2014 - 2015 CodUP (<http://codup.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,9 +19,9 @@
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from openerp import models, fields, api, _
 
-class account_invoice(osv.osv):
+class account_invoice(models.Model):
     _name = 'account.invoice'
     _inherit = 'account.invoice'
 
@@ -30,5 +30,33 @@ class account_invoice(osv.osv):
             context = {}
         context.update({'email': True})
         return super(account_invoice, self).action_invoice_sent(cr, uid, ids, context=context)
+        
+    @api.multi
+    def action_bill_sent(self):
+        """ Open a window to compose an email, with the edi invoice template
+            message loaded by default
+        """
+        assert len(self) == 1, 'This option should only be used for a single id at a time.'
+        template = self.env.ref('l10n_ru_doc.email_template_edi_bill', False)
+        compose_form = self.env.ref('mail.email_compose_message_wizard_form', False)
+        ctx = dict(
+            default_model='account.invoice',
+            default_res_id=self.id,
+            default_use_template=bool(template),
+            default_template_id=template.id,
+            default_composition_mode='comment',
+            mark_invoice_as_sent=True,
+        )
+        return {
+            'name': _('Compose Email'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form.id, 'form')],
+            'view_id': compose_form.id,
+            'target': 'new',
+            'context': ctx,
+        }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

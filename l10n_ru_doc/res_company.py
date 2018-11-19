54935c5d8afff5e1ee4fd8f6a3f6e52979caa577
wowas18 <wowas18@nm.ru>
2018-11-19 10:54:00 +0400

    fix related fields

--- l10n_ru_doc/res_company.py
+++ l10n_ru_doc/res_company.py
@@ -11,9 +11,9 @@ from odoo import api, fields, models
 class Company(models.Model):
     _inherit = 'res.company'
 
-    inn = fields.Char(related='partner_id.inn')
-    kpp = fields.Char(related='partner_id.kpp')
-    okpo = fields.Char(related='partner_id.okpo')
+    inn = fields.Char(related='partner_id.inn', readonly=False)
+    kpp = fields.Char(related='partner_id.kpp', readonly=False)
+    okpo = fields.Char(related='partner_id.okpo', readonly=False)
     chief_id = fields.Many2one('res.users', 'Chief')
     accountant_id = fields.Many2one('res.users', 'General Accountant')
     print_facsimile = fields.Boolean('Print Facsimile', help="Check this for adding Facsimiles of responsible persons to documents.")
